0b881dc3e224d8820d56b48cac3a9a0567e18287
Michael Curry <currymike89@gmail.com>
2013-11-05 14:49:56 -0500

    Lab 4 Logic

--- currymichael_lab3.js
+++ currymichael_lab3.js
@@ -1,40 +1,21 @@
-/**
- * @author Mike
- */
-var TattooArtist = "Nick Smoove";
-var ShopName = "\"Nick Smoove\" Tattoo Parlor";
-var GetTattoo = true; 
-var StartPrice = 50;
-var NumTattoo
-var NewPrice
+//Michael Curry
+//Mobile Development 
+//Scalable Data Infrastructures
+//Boolean, Loops and Conditions
+//Lab Day 4
 
-console.log ("Hello everyone my name is " + TattooArtist +" from Washington, DC.");
-console.log("I'm the owner of " + ShopName + " currently in Orlando, Fl");
-console.log("It is " + GetTattoo+ " that you want a tattoo today.");
-console.log("The starting price for any tattoo is $" + StartPrice +"");
+// for loop
 
-GetTattoo = confirm("Do you want a tattoo today?");
+//
 
-if (GetTattoo === true){
-	var YourPrice = parseInt(prompt("How much money do you have?"));
-	
-	if(YourPrice <= 50){
-	  NewPrice = (YourPrice + 50); 
-		console.log("Until you get " + NewPrice  + " call me again later!");
-	}else{
-		console.log("I would love to do some art for you today");
-	}
-}
 
-else{
-	NumTattoo = prompt("Do you have tattoos?","Yes or No");
-	if(NumTattoo === "No"){
-		console.log("Lets get some more ink on that body of yours ");
-	}
-	else if (NumTattoo === "Yes") {
-		console.log("I got several tattoos too lets see some of yours");
-	}else{
-		console.log("I dont understand your input?? Yes or No answer please!");
-	};
+while (name !="End" || name !="Stop"){
+
+
+var name = prompt("What is your name?", "name here")
+if (name !="End" || name !="Stop"){
+console.log("Wrong");
+}else{
+	console.log("Blahhh");
 };
-
+};
---
+++ logic-tables.js
@@ -0,0 +1,25 @@
+//  NAME:  Enter your name here
+//  DATE:  Enter the current date
+// Scalable Data Infrastructures
+// Day 4 Lab
+//  Assignment 1
+//  Using logical operators
+
+var p;
+var q;
+var r;
+
+p = confirm("First value:  Click OK for true or Cancel for false.");
+q = confirm("Second value:  Click OK for true or Cancel for false.");
+r = confirm("Third value: Click Ok for true or Cancel for false")
+
+{
+
+if (p || (q || r)) {
+    console.log("With " + p + " and " + q + " and " + r + "  the outcome is TRUE.");
+} else {
+    console.log("With " + p + " and " + q + " and " + r + " the outcome is FALSE.");
+};
+};
+
+
