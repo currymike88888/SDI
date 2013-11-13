7c186ea7dd1beba24232add2b24086ff8bed18c3
Michael Curry <currymike89@gmail.com>
2013-11-13 16:58:55 -0500

    Lab 7 #2 Objects

--- curry_michael_labday7json.js
+++ curry_michael_labday7json.js
@@ -40,10 +40,43 @@ var myJSONFunction = function()
     }
 };
 
-var createNFL = function() {
+var objConstructor = function(NFLteam,NFLState,NFLconference) {
     
+    this.team = NFLteam;
+    this.teamstate = NFLState;
+    this.conference = NFLconference
+    this.playoff = false;
+    this.myMethod = function(){
+        this.playoff = true;
+    };
     
-}
+};
+
+var createNFL = function(){
+    
+    for (var i = 0; i < jsonDataNFL.NFL.length; i++ ){
+        
+        team = jsonDataNFL.NFL[i].team;
+        teamstate = jsonDataNFL.NFL[i].teamstate;
+        conference = jsonDataNFL.NFL[i].conference;
+        
+        if (i === 0) {
+            objNewTeam = new objConstructor(team,teamstate,conference)
+        }else{
+            objNewTeamTwo = new objConstructor(team,teamstate,conference)
+        };
+    };
+};
+
 
 myJSONFunction();
+
+createNFL();
+
+objNewTeam.myMethod();
+console.log("This year it is", objNewTeam.playoff, "that the", objNewTeam.team, "are going to the playoffs");
+
+//objNewTeamTwo.myMethod();
+console.log("This year it is", objNewTeamTwo.playoff, "that the", objNewTeamTwo.team, "are going to the playoffs");
+
 
